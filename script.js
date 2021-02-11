775462500784abdbc14987dbd1e4675b21afedd1
Bryce Spraggins <spragbry93@gmail.com>
2021-02-11 18:27:33 -0500

    add-ons

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ let generateBtn = document.querySelector("#generate");
 let step = "password-length"
 let passwordLength 
 let characterType
+let uppercaseType
+let lowercaseType
+let numericType
+let specialType
 function displayForm(text,value) {
   value = window.prompt(text)
 }
@@ -12,11 +16,29 @@ function generatePassword() {
   password = password.toUpperCase()
   return password
 }
+function generatePassword() { //added by Bryce 
+  let password = ""
+  password = password.toLowerCase
+  return password
+}
+function generatePassword() { //added by Bryce
+  let password = ""
+  password = password.numericType
+  return password
+}
+function generatePassword() { //added by Bryce 
+  let password = ""
+  password = password.specialType
+  return password
+}
 
 // Write password to the #password input
 function writePassword() {
   displayForm("Enter a length between 8 and 128 characters",passwordLength)
-  displayForm("Enter a choice of lowercase, uppercase, numeric, and/or special characters",characterType)
+  displayForm("Enter a choice of lowercase",characterType) //would this be lowercaseType or characterType? (by Bryce )
+  displayForm("Enter a choice of uppercase",uppercaseType)
+  displayForm("Enter a Numeric value",numericType)
+  displayForm("Enter a special character",specialType)
   console.log("Password")
   let password = generatePassword();
   let passwordText = document.querySelector("#password");
